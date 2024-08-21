6d594d7493943e39b213d76c54a031cb3becd204
accessmania <soshirpost@nate.com>
2024-08-21 18:28:35 +0900

    3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 //Toast.makeText(getApplication(),"ttt111", Toast.LENGTH_LONG);
                 TextView t = (TextView) findViewById(R.id.textView);
 
-                t.setText("d");
+                t.setText("d2");
             }
         });
 
